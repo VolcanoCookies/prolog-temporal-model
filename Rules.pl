e81e31c8583e074f93ca76e5bb78d0a2f8efa0d5
VolcanoCookies <franek.kg@gmail.com>
2021-12-12 17:41:32 +0100

    fix

--- Rules.pl
+++ Rules.pl
@@ -1,6 +1,6 @@
 % Catch-All for logging
 check(_, _, State, _, Exp) :-
-    %writef('\n%t : Currently evaluating %t', [State, Exp]),
+    writef('\n%t : Currently evaluating %t', [State, Exp]),
     fail.
 
 check(_, Labels, State, _, Exp) :-
@@ -11,9 +11,9 @@ check(_, Labels, State, _, Exp) :-
 check(Transitions, Labels, State, Blocked, neg(Exp)) :-
     not(check(Transitions, Labels, State, Blocked, Exp)).
 
-check(T, L, S, Blocked, and(X1, X2)) :-
-    check(T, L, S, Blocked, X1),
-    check(T, L, S, Blocked, X2).
+check(Transitions, Labels, State, Blocked, and(Exp1, Exp2)) :-
+    check(Transitions, Labels, State, Blocked, Exp1),
+    check(Transitions, Labels, State, Blocked, Exp2).
 
 check(Transitions, Labels, State, Blocked, or(Exp1, Exp2)) :-
     (check(Transitions, Labels, State, Blocked, Exp1);
@@ -29,8 +29,7 @@ check(Transitions, Labels, State, Blocked, af(Exp)) :-
 
 check(Transitions, Labels, State, Blocked, af(Exp)) :-
     next(Transitions, State, Blocked, Next),
-    append(Blocked, State, NewBlocked),
-    all(Next, Var, check(Transitions, Labels, Var, NewBlocked, af(Exp))).
+    all(Next, Var, check(Transitions, Labels, Var, [State|Blocked], af(Exp))).
 
 check(Transitions, Labels, State, Blocked, ag(Exp)) :-
     member(State, Blocked),
@@ -39,15 +38,13 @@ check(Transitions, Labels, State, Blocked, ag(Exp)) :-
 check(Transitions, Labels, State, Blocked, ag(Exp)) :-
     not(member(State, Blocked)),
     check(Transitions, Labels, State, Blocked, Exp),
-    append(Blocked, State, NewBlocked),
-    next(Transitions, State, NewBlocked, Next),
-    all(Next, Var, check(Transitions, Labels, Var, [NewBlocked|Var], ag(Exp))).
+    next(Transitions, State, [State|Blocked], Next),
+    all(Next, Var, check(Transitions, Labels, Var, [Var|[State|Blocked]], ag(Exp))).
 
 check(Transitions, Labels, State, Blocked, ex(Exp)) :-
     next(Transitions, State, Blocked, Possible),
     member(Next, Possible),
-    append(Blocked, Next, NewBlocked),
-    check(Transitions, Labels, Next, NewBlocked, Exp).
+    check(Transitions, Labels, Next, [Next|Blocked], Exp).
 
 check(Transitions, Labels, State, Blocked, ef(Exp)) :-
     check(Transitions, Labels, State, Blocked, Exp).
